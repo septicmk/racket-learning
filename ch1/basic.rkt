1552b4b4f1637fb6b979bf67487c0209179016ef
septicmk <septic.mk@gmail.com>
2016-10-16 09:25:23 +0800

    1-6 1-7 1-8

--- ch1/basic.rkt
+++ ch1/basic.rkt
@@ -1,7 +1,6 @@
 ;;#lang typed/racket
 #lang sicp
 
-
 ;; test 1 - define
 (define size 2)
 (define pi 3.141592653)
@@ -35,5 +34,23 @@
 (define (abs_3 x)
   (if ( < x 0 ) (- x) x))
 
+;; test 3 - guess sqrt
 
+(define (sqrt-iter guess x)
+  ( if (good-enough? guess x)
+       guess
+       (sqrt-iter (improve guess x) x)))
 
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y ) 2))
+
+(define (my-sqrt x)
+  (sqrt-iter 1. x))
+
+;;(my-sqrt 9)
--- ch1/p1-1.rkt
+++ ch1/p1-1.rkt
@@ -53,16 +53,16 @@
   (+ (** (bigger a b))
      (** (bigger c (smaller a b)))))
 
-;;(sum-of-bigger-two 1 2 3)
-;;(sum-of-bigger-two 2 1 3)
-;;(sum-of-bigger-two 2 3 1)
+(sum-of-bigger-two 1 2 3)
+(sum-of-bigger-two 2 1 3)
+(sum-of-bigger-two 2 3 1)
 
 "##### 1-4 #####"
 (define (a-plus-abs-b a b)
   ( (if ( > b 0 ) + - ) a b))
 
-;;(a-plus-abs-b 1 2)
-;;(a-plus-abs-b 1 -2)
+(a-plus-abs-b 1 2)
+(a-plus-abs-b 1 -2)
 
 
 "##### 1-5 #####"
@@ -73,7 +73,64 @@
     0 
     y))
 
-;;(test 0 p)
+(test 0 p)
+
+"##### 1-6 #####"
+
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause )))
+
+
+
+(new-if #t (display "good") (display "bad"))
+(display "\n")
+(if #t (display "good") (display "bad"))
+(display "\n")
+
+"##### 1-7 #####"
+
+(define (sqrt-iter guess x)
+  ( if (good-enough? guess (improve guess x))
+       (improve guess x)
+       (sqrt-iter (improve guess x) x)))
+
+(define (good-enough? last-guess guess)
+  (< (abs (- last-guess guess)) 0.001))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y ) 2))
+
+(define (my-sqrt x)
+  (sqrt-iter 1. x))
+
+(my-sqrt 9)
+
+"##### 1-8 #####"
+
+(define (cube-root-iter guess x)
+  ( if (good-enough? guess (improve-cube guess x))
+       (improve-cube guess x)
+       (cube-root-iter (improve-cube guess x) x)))
+
+(define (improve-cube y x)
+  (/ (+ (/ x (* y y)) 
+        (* 2 y)) 
+     3))
+
+(define (cube-root x)
+  (cube-root-iter 1. x))
+
+(define (cube x)
+  (* x x x))
+
+(cube-root (cube 4))
+(cube-root (cube 2.7))
+(cube-root (cube 1))
 
 
 
